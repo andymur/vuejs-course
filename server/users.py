74ffd4179e4ac2b58943e944684a5e0f47d032ea
andymur <andymurh@gmail.com>
2019-04-22 12:58:48 +0200

    Implement editing/adding user API

--- server/users.py
+++ server/users.py
@@ -17,15 +17,32 @@ donald = {'id': 1, 'firstName': 'Donald', 'secondName': 'McDonald', 'patronymic'
 
 users = [donald, matt, lena, roger, andy]
 
+users_hash = {1: donald, 2: matt, 3: lena, 4: roger, 5: andy}
+
 @app.route('/users/')
 def users_list():
-    content = users 
+    content = list(users_hash.itervalues()) 
     response = Response(response=json.dumps(content), status=200, mimetype='application/json')
     return response
 
-@app.route('/user/<userId>')
+@app.route('/user/<int:userId>')
 def user_data(userId):
-    content = donald
-    response = Response(response=json.dumps(content), status=200, mimetype='application/json')
+    user = users_hash.get(userId)
+    if user is None:
+        raise Exception('User not found!')
+    response = Response(response=json.dumps(user), status=200, mimetype='application/json')
     return response
 
+@app.route('/useredit/<int:userId>', methods=['PATCH'])
+def user_edit(userId):
+    user = users_hash.get(userId)
+    newUser = request.json
+
+    if user is None:
+        userId = max(users_hash.keys()) + 1
+
+    newUser['id'] = userId
+
+    users_hash[userId] = newUser
+    response = Response(response=json.dumps(newUser), status=200, mimetype='application/json')
+    return response
